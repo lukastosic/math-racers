0c57d10aba170aeec9011106f31373f71489034d
Luka Stosic <lstosic@infodation.nl>
2025-11-03 21:00:15 +0100

    updated structure and build file

--- build-dist-with-docker.sh
+++ build-dist-with-docker.sh
@@ -4,9 +4,11 @@ rm -rf ./dist \
 && docker cp math-race-container:/app/dist ./dist \
 && docker rm math-race-container \
 && cd dist \
+&& git init \
 && git checkout -b gh-pages \
 && git add . \
 && git commit -m "Deploy to GitHub Pages" \
+&& git remote add origin https://github.com/lukastosic/math-racers.git \
 && git push -f origin gh-pages \
 && cd .. \
 && echo "Deployment to GitHub Pages completed successfully." \

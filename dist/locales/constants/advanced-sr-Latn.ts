932bebf632c209a5961d5678d6a4048da6ae5ab7
Luka Stosic <lstosic@infodation.nl>
2025-11-03 15:19:08 +0100

    dist publish

---
+++ dist/locales/constants/advanced-sr-Latn.ts
@@ -0,0 +1,499 @@
+import { AdvancedQuestion } from '../../../src/types';
+
+export const ADVANCED_QUESTIONS: AdvancedQuestion[] = [
+  {
+    question: "Imaš 5 autića, a drug ti da još 3. Koliko autića imaš sada?",
+    answers: [
+      { answer: "8", correct: true },
+      { answer: "2", correct: false },
+      { answer: "5", correct: false },
+      { answer: "9", correct: false },
+    ],
+  },
+  {
+    question: "Sara je ispekla 6 kolačića, a njen brat 7. Koliko kolačića su ispekli zajedno?",
+    answers: [
+      { answer: "13", correct: true },
+      { answer: "1", correct: false },
+      { answer: "12", correct: false },
+      { answer: "14", correct: false },
+    ],
+  },
+  {
+    question: "Na drvetu je 8 ptica. Doletele su još 4 ptice. Koliko ptica je sada na drvetu?",
+    answers: [
+      { answer: "12", correct: true },
+      { answer: "4", correct: false },
+      { answer: "11", correct: false },
+      { answer: "13", correct: false },
+    ],
+  },
+  {
+    question: "U parku si našao 10 sjajnih kamenčića. Na putu do kuće našao si još 5. Koliko kamenčića imaš ukupno?",
+    answers: [
+      { answer: "15", correct: true },
+      { answer: "5", correct: false },
+      { answer: "10", correct: false },
+      { answer: "20", correct: false },
+    ],
+  },
+  {
+    question: "Mama ti je dala 12 sličica, a tata ti je dao 8. Koliko sličica imaš sada?",
+    answers: [
+      { answer: "20", correct: true },
+      { answer: "4", correct: false },
+      { answer: "19", correct: false },
+      { answer: "22", correct: false },
+    ],
+  },
+  {
+    question: "Imao si 10 balona, ali 3 su odletela. Koliko balona je ostalo?",
+    answers: [
+      { answer: "7", correct: true },
+      { answer: "13", correct: false },
+      { answer: "8", correct: false },
+      { answer: "6", correct: false },
+    ],
+  },
+  {
+    question: "U akvarijumu je bilo 15 ribica. Dao si 5 ribica drugu. Koliko ribica je ostalo?",
+    answers: [
+      { answer: "10", correct: true },
+      { answer: "20", correct: false },
+      { answer: "5", correct: false },
+      { answer: "12", correct: false },
+    ],
+  },
+  {
+    question: "Pica ima 8 parčića. Ti i tvoji drugari ste pojeli 6 parčića. Koliko je parčića ostalo?",
+    answers: [
+      { answer: "2", correct: true },
+      { answer: "14", correct: false },
+      { answer: "8", correct: false },
+      { answer: "3", correct: false },
+    ],
+  },
+  {
+    question: "Imaš 20 bombona. Ako pojedeš 9, koliko ti je ostalo?",
+    answers: [
+      { answer: "11", correct: true },
+      { answer: "29", correct: false },
+      { answer: "10", correct: false },
+      { answer: "12", correct: false },
+    ],
+  },
+  {
+    question: "U kutiji je 18 olovaka. Ako je 5 olovaka slomljeno, koliko je ispravnih olovaka ostalo?",
+    answers: [
+      { answer: "13", correct: true },
+      { answer: "23", correct: false },
+      { answer: "12", correct: false },
+      { answer: "14", correct: false },
+    ],
+  },
+  {
+    question: "Mačka ima 4 noge. Koliko nogu imaju 3 mačke zajedno?",
+    answers: [
+      { answer: "12", correct: true },
+      { answer: "7", correct: false },
+      { answer: "9", correct: false },
+      { answer: "10", correct: false },
+    ],
+  },
+  {
+    question: "Bicikl ima 2 točka. Koliko točkova ima 5 bicikala?",
+    answers: [
+      { answer: "10", correct: true },
+      { answer: "7", correct: false },
+      { answer: "2", correct: false },
+      { answer: "8", correct: false },
+    ],
+  },
+  {
+    question: "Svakog dana dobiješ 3 sličice. Koliko sličica ćeš imati posle 4 dana?",
+    answers: [
+      { answer: "12", correct: true },
+      { answer: "7", correct: false },
+      { answer: "3", correct: false },
+      { answer: "10", correct: false },
+    ],
+  },
+  {
+    question: "Morska zvezda ima 5 krakova. Koliko krakova imaju 3 morske zvezde?",
+    answers: [
+      { answer: "15", correct: true },
+      { answer: "8", correct: false },
+      { answer: "12", correct: false },
+      { answer: "16", correct: false },
+    ],
+  },
+  {
+    question: "U jednoj kutiji ima 6 jaja. Koliko jaja ima u 2 kutije?",
+    answers: [
+      { answer: "12", correct: true },
+      { answer: "8", correct: false },
+      { answer: "6", correct: false },
+      { answer: "10", correct: false },
+    ],
+  },
+  {
+    question: "Ti imaš 5 evra, a tvoja sestra 6 evra. Možete li zajedno da kupite igračku koja košta 12 evra?",
+    answers: [
+      { answer: "Ne", correct: true },
+      { answer: "Da", correct: false },
+      { answer: "Možda", correct: false },
+      { answer: "Ja imam dovoljno", correct: false },
+    ],
+  },
+  {
+    question: "Knjiga košta 10 evra. Ti imaš 7 evra. Koliko ti još evra treba da je kupiš?",
+    answers: [
+      { answer: "3 evra", correct: true },
+      { answer: "17 evra", correct: false },
+      { answer: "7 evra", correct: false },
+      { answer: "10 evra", correct: false },
+    ],
+  },
+  {
+    question: "Autić košta 8 evra. Platio si novčanicom od 10 evra. Koliki kusur ćeš dobiti?",
+    answers: [
+      { answer: "2 evra", correct: true },
+      { answer: "18 evra", correct: false },
+      { answer: "8 evra", correct: false },
+      { answer: "3 evra", correct: false },
+    ],
+  },
+  {
+    question: "Potrebno ti je 20 poena da pobediš u igri. Ti imaš 15 poena. Koliko ti još poena treba?",
+    answers: [
+      { answer: "5", correct: true },
+      { answer: "35", correct: false },
+      { answer: "10", correct: false },
+      { answer: "20", correct: false },
+    ],
+  },
+  {
+    question: "Leo ima 12 bojica. Mia ima 3 bojice više od Lea. Koliko bojica ima Mia?",
+    answers: [
+      { answer: "15", correct: true },
+      { answer: "9", correct: false },
+      { answer: "12", correct: false },
+      { answer: "16", correct: false },
+    ],
+  },
+  {
+    question: "Autobus ima 25 sedišta. Sedi 18 ljudi. Koliko je sedišta prazno?",
+    answers: [
+      { answer: "7", correct: true },
+      { answer: "43", correct: false },
+      { answer: "8", correct: false },
+      { answer: "6", correct: false },
+    ],
+  },
+  {
+    question: "Imaš 3 crvena klikera, 5 plavih i 4 zelena. Koliko klikera imaš ukupno?",
+    answers: [
+      { answer: "12", correct: true },
+      { answer: "8", correct: false },
+      { answer: "9", correct: false },
+      { answer: "10", correct: false },
+    ],
+  },
+  {
+    question: "Farmer ima 10 kokošaka i 5 krava. Koliko životinja ima ukupno?",
+    answers: [
+      { answer: "15", correct: true },
+      { answer: "5", correct: false },
+      { answer: "10", correct: false },
+      { answer: "20", correct: false },
+    ],
+  },
+  {
+    question: "U ponedeljak si pročitao 8 strana knjige, a u utorak 9. Koliko si strana pročitao za dva dana?",
+    answers: [
+      { answer: "17", correct: true },
+      { answer: "1", correct: false },
+      { answer: "16", correct: false },
+      { answer: "18", correct: false },
+    ],
+  },
+  {
+    question: "Veliki pas ima 15 kučića. Mali pas ima 6 kučića. Koliko kučića više ima veliki pas?",
+    answers: [
+      { answer: "9", correct: true },
+      { answer: "21", correct: false },
+      { answer: "8", correct: false },
+      { answer: "10", correct: false },
+    ],
+  },
+  {
+    question: "U odeljenju ima 20 dece. 11 su dečaci. Koliko ima devojčica?",
+    answers: [
+      { answer: "9", correct: true },
+      { answer: "31", correct: false },
+      { answer: "10", correct: false },
+      { answer: "8", correct: false },
+    ],
+  },
+  {
+    question: "Imaš 2 džepa. U svaki džep staviš 6 novčića. Koliko novčića imaš?",
+    answers: [
+      { answer: "12", correct: true },
+      { answer: "8", correct: false },
+      { answer: "10", correct: false },
+      { answer: "14", correct: false },
+    ],
+  },
+  {
+    question: "Na drvetu je 14 jabuka. 5 je palo. Koliko jabuka je ostalo na drvetu?",
+    answers: [
+      { answer: "9", correct: true },
+      { answer: "19", correct: false },
+      { answer: "10", correct: false },
+      { answer: "8", correct: false },
+    ],
+  },
+  {
+    question: "Imaš 3 druga i želiš svakom da daš 4 bombone. Koliko ti bombona treba?",
+    answers: [
+      { answer: "12", correct: true },
+      { answer: "7", correct: false },
+      { answer: "10", correct: false },
+      { answer: "11", correct: false },
+    ],
+  },
+  {
+    question: "Voz ima 5 vagona. Svaki vagon ima 4 prozora. Koliko prozora ima ukupno?",
+    answers: [
+      { answer: "20", correct: true },
+      { answer: "9", correct: false },
+      { answer: "15", correct: false },
+      { answer: "24", correct: false },
+    ],
+  },
+  {
+    question: "Imaš 18 sličica. Dao si 5 bratu i 3 sestri. Koliko ti je ostalo?",
+    answers: [
+      { answer: "10", correct: true },
+      { answer: "8", correct: false },
+      { answer: "13", correct: false },
+      { answer: "26", correct: false },
+    ],
+  },
+  {
+    question: "Nedelja ima 7 dana. Koliko dana imaju 3 nedelje?",
+    answers: [
+      { answer: "21", correct: true },
+      { answer: "10", correct: false },
+      { answer: "14", correct: false },
+      { answer: "20", correct: false },
+    ],
+  },
+  {
+    question: "Želiš da kupiš strip koji košta 15 evra. Mama ti da 10 evra. Koliko treba da tražiš od tate?",
+    answers: [
+      { answer: "5 evra", correct: true },
+      { answer: "25 evra", correct: false },
+      { answer: "10 evra", correct: false },
+      { answer: "6 evra", correct: false },
+    ],
+  },
+  {
+    question: "Ako imaš 3 reda cveća sa po 5 cvetova u svakom redu, koliko cveća imaš?",
+    answers: [
+      { answer: "15", correct: true },
+      { answer: "8", correct: false },
+      { answer: "12", correct: false },
+      { answer: "16", correct: false },
+    ],
+  },
+  {
+    question: "Ima 24 kolačića. Ako ih podeliš jednako na 4 druga, koliko će svako dobiti?",
+    answers: [
+      { answer: "6", correct: true },
+      { answer: "4", correct: false },
+      { answer: "5", correct: false },
+      { answer: "8", correct: false },
+    ],
+  },
+  {
+    question: "Pauk ima 8 nogu. Koliko nogu imaju 3 pauka?",
+    answers: [
+      { answer: "24", correct: true },
+      { answer: "11", correct: false },
+      { answer: "16", correct: false },
+      { answer: "32", correct: false },
+    ],
+  },
+  {
+    question: "Imaš 30 centi. Jabuka košta 10 centi. Koliko jabuka možeš da kupiš?",
+    answers: [
+      { answer: "3", correct: true },
+      { answer: "2", correct: false },
+      { answer: "4", correct: false },
+      { answer: "30", correct: false },
+    ],
+  },
+  {
+    question: "Školski autobus ima 12 prozora sa jedne strane i 12 sa druge. Koliko prozora ima ukupno?",
+    answers: [
+      { answer: "24", correct: true },
+      { answer: "12", correct: false },
+      { answer: "22", correct: false },
+      { answer: "26", correct: false },
+    ],
+  },
+  {
+    question: "U bašti ima 7 crvenih cvetova, 5 žutih i 8 roze cvetova. Koliko cvetova ima ukupno?",
+    answers: [
+      { answer: "20", correct: true },
+      { answer: "12", correct: false },
+      { answer: "13", correct: false },
+      { answer: "22", correct: false },
+    ],
+  },
+  {
+    question: "Toma ima 8 godina. Njegova sestra je 4 godine mlađa. Koliko godina ima njegova sestra?",
+    answers: [
+      { answer: "4", correct: true },
+      { answer: "12", correct: false },
+      { answer: "8", correct: false },
+      { answer: "2", correct: false },
+    ],
+  },
+  {
+    question: "Na igralištu je 25 dece. 10 ode kući. Zatim dođe još 5. Koliko dece je sada na igralištu?",
+    answers: [
+      { answer: "20", correct: true },
+      { answer: "15", correct: false },
+      { answer: "30", correct: false },
+      { answer: "25", correct: false },
+    ],
+  },
+  {
+    question: "Farmer ima 14 ovaca. Prodao je polovinu. Koliko mu je ovaca ostalo?",
+    answers: [
+      { answer: "7", correct: true },
+      { answer: "14", correct: false },
+      { answer: "28", correct: false },
+      { answer: "6", correct: false },
+    ],
+  },
+  {
+    question: "Ako film počinje u 3 sata i traje 2 sata, u koliko sati se završava?",
+    answers: [
+      { answer: "5 sati", correct: true },
+      { answer: "1 sat", correct: false },
+      { answer: "3 sata", correct: false },
+      { answer: "4 sata", correct: false },
+    ],
+  },
+  {
+    question: "Imaš 4 druga i 16 kolačića da podelite jednako. Koliko kolačića svako dobija?",
+    answers: [
+      { answer: "4", correct: true },
+      { answer: "12", correct: false },
+      { answer: "20", correct: false },
+      { answer: "5", correct: false },
+    ],
+  },
+  {
+    question: "U kutiji se nalazi 5 plavih, 5 crvenih i 5 zelenih loptica. Koliko loptica ima u kutiji?",
+    answers: [
+      { answer: "15", correct: true },
+      { answer: "10", correct: false },
+      { answer: "5", correct: false },
+      { answer: "20", correct: false },
+    ],
+  },
+  {
+    question: "Mesec ima oko 4 nedelje. Koliko nedelja ima u dva meseca?",
+    answers: [
+      { answer: "8", correct: true },
+      { answer: "4", correct: false },
+      { answer: "6", correct: false },
+      { answer: "10", correct: false },
+    ],
+  },
+  {
+    question: "Ako jedeš 2 jabuke svakog dana, koliko ćeš jabuka pojesti za nedelju dana (7 dana)?",
+    answers: [
+      { answer: "14", correct: true },
+      { answer: "9", correct: false },
+      { answer: "7", correct: false },
+      { answer: "10", correct: false },
+    ],
+  },
+  {
+    question: "Na fudbalskom terenu su 22 igrača. 11 je iz tvog tima. Koliko ih je iz drugog tima?",
+    answers: [
+      { answer: "11", correct: true },
+      { answer: "22", correct: false },
+      { answer: "0", correct: false },
+      { answer: "33", correct: false },
+    ],
+  },
+  {
+    question: "Na drvetu je bilo 20 listova. Vetar je oduvao 8. Koliko je listova ostalo na drvetu?",
+    answers: [
+      { answer: "12", correct: true },
+      { answer: "28", correct: false },
+      { answer: "10", correct: false },
+      { answer: "15", correct: false },
+    ],
+  },
+  {
+    question: "Štediš 5 evra svake nedelje. Koliko ćeš novca imati za 4 nedelje?",
+    answers: [
+      { answer: "20 evra", correct: true },
+      { answer: "9 evra", correct: false },
+      { answer: "15 evra", correct: false },
+      { answer: "25 evra", correct: false },
+    ],
+  },
+  {
+    question: "U torbi su 3 jabuke, 4 banane i 2 pomorandže. Koliko voća ima u torbi?",
+    answers: [
+      { answer: "9", correct: true },
+      { answer: "7", correct: false },
+      { answer: "6", correct: false },
+      { answer: "8", correct: false },
+    ],
+  },
+  {
+    question: "Ima 10 pari cipela. Koliko je to pojedinačnih cipela?",
+    answers: [
+      { answer: "20", correct: true },
+      { answer: "10", correct: false },
+      { answer: "12", correct: false },
+      { answer: "30", correct: false },
+    ],
+  },
+  {
+    question: "Kokoška snese 3 jajeta dnevno. Koliko jaja snese za 5 dana?",
+    answers: [
+      { answer: "15", correct: true },
+      { answer: "8", correct: false },
+      { answer: "12", correct: false },
+      { answer: "20", correct: false },
+    ],
+  },
+  {
+    question: "Počinješ sa 15 sličica. Izgubiš 6, ali onda osvojiš 4. Koliko sličica imaš sada?",
+    answers: [
+      { answer: "13", correct: true },
+      { answer: "9", correct: false },
+      { answer: "19", correct: false },
+      { "answer": "11", "correct": false }
+    ],
+  },
+  {
+    question: "Četiri druga donesu po 3 knjige u školu. Koliko knjiga imaju ukupno?",
+    answers: [
+      { answer: "12", correct: true },
+      { answer: "7", correct: false },
+      { answer: "10", correct: false },
+      { answer: "15", correct: false },
+    ],
+  },
+];
